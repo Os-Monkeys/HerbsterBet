9fc9fb5c81c6000723b28e046c73fcc2d2c0bd0f
Jorge <jorginhovscardozo@gmail.com>
2025-11-05 10:23:10 -0300

    cacaniquel metade

--- cacaNiquel.py
+++ cacaNiquel.py
@@ -1,4 +1,5 @@
 import random
+from flask import Flask, render_template, request, flash, url_for, session 
 #formar um caça-níquel
 def rodarRoleta():
     numero = random.randint(1, 10)
@@ -11,14 +12,15 @@ simboloTres = rodarRoleta()
 
 
 
+print(f'[{simboloUm}][{simboloDois}][{simboloTres}]')
+if simboloUm == simboloDois and simboloUm == simboloTres: #Jackpot
+    adicional = 50.50
+    money += adicional
+elif simboloUm == simboloDois or simboloTres == simboloUm or simboloDois == simboloTres: #Dois iguais
+    adicional = 5.00
+    money += adicional
+else: #Todos diferentes
+    adicional = 0.00
+    money += adicional
 
-
-
-
-
-
-
-
-#Jackpot
-if simboloUm == simboloDois and simboloUm == simboloTres:
-    money += 0.50
+print(f"Você recebeu R${adicional:.2f}!!!")
