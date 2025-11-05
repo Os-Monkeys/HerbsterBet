3a8c5d46c034d22311ed49542541b9a82bb98049
ReiDosGnomos <hippohipopotamo1962@gmail.com>
2025-11-05 16:21:28 -0300

    Faz O Negocio

--- cacaNiquel.py
+++ cacaNiquel.py
@@ -1,14 +1,24 @@
 import random
 from flask import Flask, render_template, request, flash, url_for, session, redirect
 
+array = dict(zip(['jusbiscreudo','Enxi','JesseChad'],[1,2,3]))
+
 app = Flask(__name__)
 app.secret_key = "qualquer_coisa"
 
 def rodarRoleta():
     return random.randint(1, 10)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
 @app.route('/')
 def index():
+    if user not in session:
+        return redirect(url_for('login.html'))
     if 'money' not in session:
         session['money'] = 500.00
     return render_template('index.html', money=session['money'])
