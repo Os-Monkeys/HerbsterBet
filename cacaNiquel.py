31307bda4eced929a758886f19609296525586b8
Jorge <jorginhovscardozo@gmail.com>
2025-11-05 11:56:47 -0300

    cacaniquel metade

--- cacaNiquel.py
+++ cacaNiquel.py
@@ -1,26 +1,39 @@
 import random
 from flask import Flask, render_template, request, flash, url_for, session 
+
+app = Flask(__name__)
+
 #formar um caça-níquel
 def rodarRoleta():
     numero = random.randint(1, 10)
     return numero
 
 money = 500.00
+
 simboloUm = rodarRoleta()
 simboloDois = rodarRoleta()
 simboloTres = rodarRoleta()
 
 
+@app.route('/')
+def index():
+    return render_template('index')
 
-print(f'[{simboloUm}][{simboloDois}][{simboloTres}]')
-if simboloUm == simboloDois and simboloUm == simboloTres: #Jackpot
-    adicional = 50.50
-    money += adicional
-elif simboloUm == simboloDois or simboloTres == simboloUm or simboloDois == simboloTres: #Dois iguais
-    adicional = 5.00
-    money += adicional
-else: #Todos diferentes
-    adicional = 0.00
-    money += adicional
+@app.route("/Clique", methods=['POST'])
+def girar():
+    simboloUm = rodarRoleta()
+    simboloDois = rodarRoleta()
+    simboloTres = rodarRoleta()
 
-print(f"Você recebeu R${adicional:.2f}!!!")
+    print(f'[{simboloUm}][{simboloDois}][{simboloTres}]')
+    if simboloUm == simboloDois and simboloUm == simboloTres: #Jackpot
+        adicional = 50.50
+        money += adicional
+    elif simboloUm == simboloDois or simboloTres == simboloUm or simboloDois == simboloTres: #Dois iguais
+        adicional = 5.00
+        money += adicional
+    else: #Todos diferentes
+        adicional = 0.00
+        money += adicional
+
+    print(f"Você recebeu R${adicional:.2f}!!!")
